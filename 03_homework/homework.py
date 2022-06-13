7401797abc58a6915ea008758b1a8f4a2e2bc81f
Tomoya Nakayama <t-nakayama@nssys.co.jp>
2022-06-14 01:14:39 +0900

    week3 homework

---
+++ 03_homework/homework.py
@@ -0,0 +1,112 @@
+import datetime
+import pickle
+
+import pandas as pd
+
+from prefect import flow, task, get_run_logger
+from prefect.deployments import DeploymentSpec
+from prefect.orion.schemas.schedules import CronSchedule
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+@task
+def read_data(path):
+    df = pd.read_parquet(path)
+    return df
+
+@task
+def prepare_features(df, categorical, train=True):
+    logger = get_run_logger()
+    df['duration'] = df.dropOff_datetime - df.pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    mean_duration = df.duration.mean()
+    if train:
+        logger.info(f"The mean duration of training is {mean_duration}")
+    else:
+        logger.info(f"The mean duration of validation is {mean_duration}")
+    
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    return df
+
+@task
+def train_model(df, categorical):
+    logger = get_run_logger()
+    train_dicts = df[categorical].to_dict(orient='records')
+    dv = DictVectorizer()
+    X_train = dv.fit_transform(train_dicts) 
+    y_train = df.duration.values
+
+    logger.info(f"The shape of X_train is {X_train.shape}")
+    logger.info(f"The DictVectorizer has {len(dv.feature_names_)} features")
+
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+    y_pred = lr.predict(X_train)
+    mse = mean_squared_error(y_train, y_pred, squared=False)
+    logger.info(f"The MSE of training is: {mse}")
+    return lr, dv
+
+@task
+def run_model(df, categorical, dv, lr):
+    logger = get_run_logger()
+    val_dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(val_dicts) 
+    y_pred = lr.predict(X_val)
+    y_val = df.duration.values
+
+    mse = mean_squared_error(y_val, y_pred, squared=False)
+    logger.info(f"The MSE of validation is: {mse}")
+    return
+
+@task
+def get_paths(date:str=None):
+    logger = get_run_logger()
+    if str is None:
+        base_date = datetime.date.today()
+    else:
+        base_date = datetime.date.fromisoformat(date)
+    train_date = base_date - datetime.timedelta(days=60)
+    valid_date = base_date - datetime.timedelta(days=30)
+    train_path = f'./data/fhv_tripdata_{train_date.strftime("%Y-%m")}.parquet'
+    valid_path = f'./data/fhv_tripdata_{valid_date.strftime("%Y-%m")}.parquet'
+    logger.info(f"Train data path:  {train_path}")
+    logger.info(f"Valid data path:  {valid_path}")
+    return train_path, valid_path
+
+
+#def main(train_path: str = './data/fhv_tripdata_2021-01.parquet', 
+#           val_path: str = './data/fhv_tripdata_2021-02.parquet'):
+@flow
+def main(date=None):
+    train_path, val_path = get_paths(date).result()
+
+    categorical = ['PUlocationID', 'DOlocationID']
+
+    df_train = read_data(train_path)
+    df_train_processed = prepare_features(df_train, categorical)
+
+    df_val = read_data(val_path)
+    df_val_processed = prepare_features(df_val, categorical, False)
+
+    # train the model
+    lr, dv = train_model(df_train_processed, categorical).result()
+    run_model(df_val_processed, categorical, dv, lr)
+
+    with open(f'models/model-{date}.bin', 'wb') as f:
+        pickle.dump(lr, f)
+    with open(f'models/dv-{date}.b', 'wb') as f:
+        pickle.dump(dv, f)
+
+main(date="2021-08-15")
+
+#DeploymentSpec(
+#    name="homework",
+#    flow=main,
+#    schedule=CronSchedule(
+#        cron="0 9 15 * *",
+#        timezone="Asia/Tokyo"),
+#)
